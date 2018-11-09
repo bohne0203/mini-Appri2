d144b5c2fe3233cb219d19e5617d0ddd270360e5
Ko Watanabe <lsbean82@gmail.com>
2018-11-09 13:51:02 +0900

    Create user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181109044041) do
+ActiveRecord::Schema.define(version: 20181109044825) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title",      null: false
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20181109044041) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.text     "email",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name"
   end
 
 end
